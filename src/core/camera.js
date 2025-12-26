cbf033a484ad75695183c8e06609861a1a380d51
Miguel Angel <eti.gonzalez.1980@gmail.com>
2025-12-26 03:40:00 +0100

    Update camera.js

--- src/core/camera.js
+++ src/core/camera.js
@@ -1,1 +1,10 @@
+export async function startCamera() {
+  const video = document.getElementById('video');
 
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: { ideal: "environment" } },
+    audio: false
+  });
+
+  video.srcObject = stream;
+}
